a7e4c04389280220ff7f3040620d01ba68ea3aae
Thierry Jacoel <thierry.jacoel@gmail.com>
2025-07-02 06:45:41 +0200

    feat(front): Changed contact form with obfuscated email

--- front/src/components/footer/Footer.tsx
+++ front/src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import { Stack } from "@mui/material"
 import { useState } from "react"
 import TosDialog from "./TosDialog"
 import PrivacyDialog from "./PrivacyDialog"
-import ContactDialog from "./ContactDialog"
+// import ContactDialog from "./ContactDialog"
 // import CookiesDialog from "./CookiesDialog"
 import AboutDialog from "./AboutDialog"
 
@@ -13,7 +13,7 @@ export default function Footer({ sx }: { sx?: React.CSSProperties }) {
   const [isContactVisible, setIsContactVisible] = useState(false)
   const [isAboutVisible, setIsAboutVisible] = useState(false)
 
-  const [contactSubject, setContactSubject] = useState("")
+  // const [contactSubject, setContactSubject] = useState("")
 
 
   return (
@@ -31,7 +31,13 @@ export default function Footer({ sx }: { sx?: React.CSSProperties }) {
           {/* <a href="#" onClick={() => setIsCookiesVisible(true)}>Cookie Policy</a> */}
         </Stack>
         <Stack direction="column" alignItems="center">
-          <a href="#" onClick={() => { setContactSubject(""); setIsContactVisible(true) }}>Contact Us</a>
+          Contact Us:
+          <a
+            href="mailto:&#099;&#111;&#110;&#116;&#097;&#099;&#116;&#046;&#109;&#105;&#110;&#101;&#099;&#114;&#097;&#102;&#116;&#115;&#101;&#114;&#118;&#101;&#114;&#101;&#120;&#112;&#108;&#111;&#114;&#101;&#114;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;"
+            target="_blank">
+            &#099;&#111;&#110;&#116;&#097;&#099;&#116;&#046;&#109;&#105;&#110;&#101;&#099;&#114;&#097;&#102;&#116;&#115;&#101;&#114;&#118;&#101;&#114;&#101;&#120;&#112;&#108;&#111;&#114;&#101;&#114;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;
+          </a>
+
           <a href="#" onClick={() => setIsAboutVisible(true)}>About Us</a>
         </Stack>
       </Stack>
@@ -39,11 +45,11 @@ export default function Footer({ sx }: { sx?: React.CSSProperties }) {
       <TosDialog isVisible={isTosVisible} setIsVisible={setIsTosVisible} />
       <PrivacyDialog isVisible={isPrivacyVisible} setIsVisible={setIsPrivacyVisible} />
       {/* <CookiesDialog isVisible={isCookiesVisible} setIsVisible={setIsCookiesVisible} /> */}
-      <ContactDialog
+      {/* <ContactDialog
         isVisible={isContactVisible}
         setIsVisible={setIsContactVisible}
         initialSubject={contactSubject}
-      />
+      /> */}
       <AboutDialog isVisible={isAboutVisible} setIsVisible={setIsAboutVisible} />
     </footer>
   )
